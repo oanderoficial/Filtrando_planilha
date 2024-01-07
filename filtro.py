7219ef6ca1623809c0344e4c8a4f65124ecd9f87
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2024-01-07 20:13:17 -0300

    Filtrando e exibindo colunas de uma planilha
    Filtrando e exibindo colunas de uma planilha utilizando a biblioteca pandas.

---
+++ filtro.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#Configurar para exibir todas as linhas e colunas
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+
+#carregando a planilha
+planilha = ('historicotaxajurosdiario_TodosCampos.csv')
+colunas = pd.read_csv(planilha, delimiter=';')
+
+#Filtrando as colunas que vão aparecer
+colunas_exibidas = colunas[['Posicao','InstituicaoFinanceira', 'TaxaJurosAoMes', 'TaxaJurosAoAno']]
+print(colunas_exibidas)
+
